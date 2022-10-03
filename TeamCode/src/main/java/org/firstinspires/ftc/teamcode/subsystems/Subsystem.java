6e6b20ea115cdff04e9377fdb21301165e44f892
FriedCircuits21440 <114955199+FriedCircuits21440@users.noreply.github.com>
2022-10-03 16:57:14 -0400

    Added Lift Subsystem.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Subsystem.java
@@ -6,9 +6,11 @@ public class Subsystem {
     public static Hardware robot = new Hardware();
     public static double left_motor_command;
     public static double right_motor_command;
+    public static double lift_motor_command;
 
     public static void set_motor_powers() {
         robot.leftMotor.setPower(left_motor_command);
         robot.rightMotor.setPower(right_motor_command);
+        robot.liftMotor.setPower(lift_motor_command);
     }
 }
