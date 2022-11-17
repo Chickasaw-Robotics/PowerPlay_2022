b93e68378970d4843fc242228e26498fd92e73fb
Talitha Roper <r70talshuri@yahoo.com>
2022-11-17 17:55:46 -0500

    Readjusted Collector Servo Positions

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/utils/Constants.java
@@ -9,10 +9,10 @@ public class Constants {
 
     // Preset positions
     public static final double COLLECTOR_CLOSE_POS = 1.0;
-    public static final double COLLECTOR_OPEN_POS = 0.6;
-    public static final int LIFT_HIGH_POS = 40;
-    public static final int LIFT_MID_POS = 30;
-    public static final int LIFT_LOW_POS = 20;
+    public static final double COLLECTOR_OPEN_POS = 0.8;
+    public static final int LIFT_HIGH_POS = 35;
+    public static final int LIFT_MID_POS = 25;
+    public static final int LIFT_LOW_POS = 15;
     public static final int LIFT_BOT_POS = 0;
 
     // Hardware ports
